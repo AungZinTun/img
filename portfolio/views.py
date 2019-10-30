66677bd4bdc57846fdd0f868a73ba3e67171380e
AungZinTun <42179343+AungZinTun@users.noreply.github.com>
2019-10-31 02:18:54 +0630

    index

--- portfolio/views.py
+++ portfolio/views.py
@@ -7,10 +7,10 @@ from .models import Category, Portfolio
 
 class PortfolioListView(ListView):
     model=Category
-    template_name='Portfolio/index.html'
+    template_name='portfolio/index.html'
 
 def PortfolioDetailView(request, slug):
    
     category = Category.objects.get(slug=slug)
     portfolio=Portfolio.objects.filter(category=category.id)
-    return render(request, 'Portfolio/detail.html', {'category': category, 'portfolio':portfolio})
+    return render(request, 'portfolio/detail.html', {'category': category, 'portfolio':portfolio})
